7d1310891d3dc7a5a169ea8599f4d44573b353ce
bdn1234-ai <doanbao033@gmail.com>
2026-04-29 08:21:58 +0700

    Comment out astar execution and print result
    Comment out the execution of astar and print statements.

--- algorithms/astar/astar.py
+++ algorithms/astar/astar.py
@@ -65,7 +65,7 @@ def _reconstruct(came_from: dict, current: str) -> list[str]:
         path.append(current)
     return list(reversed(path))
 
-data = GraphData()
-result = astar(data, "Central", "Hong Kong")
+#data = GraphData()
+#result = astar(data, "Central", "Hong Kong")
 
-print(result, data.heuristic("Fo Tan", "Tin Hau"))
+#print(result, data.heuristic("Fo Tan", "Tin Hau"))
